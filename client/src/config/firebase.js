9e54785bec45f008d6e5dda6a0ddb93ec4eda823
mtreerungroj <m.treerungroj@outlook.com>
2017-06-13 17:45:37 +0700

    initial commit

---
+++ client/src/config/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyA3iSHbBSRBMGCvxsmHzQWG296s0j0vcCo",
+    authDomain: "quizchatbot-ce222.firebaseapp.com",
+    databaseURL: "https://quizchatbot-ce222.firebaseio.com",
+    projectId: "quizchatbot-ce222",
+    storageBucket: "quizchatbot-ce222.appspot.com",
+    messagingSenderId: "590375043489"
+}
+
+firebase.initializeApp(config)
+
+export const ref = firebase.database().ref()
+// export const firebaseAuth = firebase.auth
