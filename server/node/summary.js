27f9983a34fe59460be93032040cb03e58bceae6
zigext <zigext@gmail.com>
2017-06-23 15:49:20 +0700

    Grade calculation with +/-

--- server/node/summary.js
+++ server/node/summary.js
@@ -1,6 +1,6 @@
 const prepareSummary = (done, keys, round, skill, grade, score) => {
     let summaryObj = {}
-    
+
     summaryObj.done = done
     summaryObj.round = round
     summaryObj.keysQuestionLeft = keys
@@ -14,11 +14,11 @@ const prepareSummary = (done, keys, round, skill, grade, score) => {
 const calculateScoreForThatQuestion = (point, result, duration) => {
     let score = 0
     //Correct
-    if(result){
-        if(duration <= 60000)   score = point + 5 //answer within 1 min
-        else if(duration > 60000)   score = point + 3 //answer not within 1 min
+    if (result) {
+        if (duration <= 60000) score = point + 5 //answer within 1 min
+        else if (duration > 60000) score = point + 3 //answer not within 1 min
     }
-   
+
     return score
 }
 
@@ -29,13 +29,21 @@ const calculateTotalScore = (totalQuestions) => {
 
 const calculateGrade = (totalScore, userScore) => {
     let grade
-    if(userScore >= totalScore*80/100)  grade = 'A'
-    else if(userScore >= totalScore*70/100)  grade = 'B'
-    else if(userScore >= totalScore*60/100)  grade = 'C'
-    else if(userScore >= totalScore*50/100)  grade = 'D'
+    if (userScore >= totalScore * 86 / 100) grade = 'A+'
+    else if (userScore >= totalScore * 83 / 100) grade = 'A'
+    else if (userScore >= totalScore * 80 / 100) grade = 'A-'
+    else if (userScore >= totalScore * 76 / 100) grade = 'B+'
+    else if (userScore >= totalScore * 73 / 100) grade = 'B'
+    else if (userScore >= totalScore * 70 / 100) grade = 'B-'
+    else if (userScore >= totalScore * 66 / 100) grade = 'C+'
+    else if (userScore >= totalScore * 63 / 100) grade = 'C'
+    else if (userScore >= totalScore * 60 / 100) grade = 'C-'
+    else if (userScore >= totalScore * 56 / 100) grade = 'D+'
+    else if (userScore >= totalScore * 53 / 100) grade = 'D'
+    else if (userScore >= totalScore * 50 / 100) grade = 'D-'
     else grade = 'F'
 
     return grade
 }
 
-module.exports = {prepareSummary, calculateTotalScore, calculateGrade, calculateScoreForThatQuestion}
+module.exports = { prepareSummary, calculateTotalScore, calculateGrade, calculateScoreForThatQuestion }
