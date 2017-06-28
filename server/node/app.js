51a96c5266638359cb71038ed8f61364b90fc33e
zigext <zigext@gmail.com>
2017-06-28 12:54:41 +0700

    test pause

--- server/node/app.js
+++ server/node/app.js
@@ -488,7 +488,7 @@ const app = async () => {
             if (userState.state === "pause") {
               console.log("_________PAUSE__________")
               let tmpDone = await getDoneFromThatUser(senderID)
-              let tmpRound = await getState(senderID)
+              let tmpRound = await getRoundFromThatUser(senderID)
               console.log("tmpRound after pause= ", tmpRound)
               setState(senderID, { "state": "playing", keysLeftForThatUser, "round": tmpRound, "done" : tmpDone })
             }
