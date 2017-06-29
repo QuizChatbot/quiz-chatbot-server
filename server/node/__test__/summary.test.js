72bb91c11bb1f2a0487e1f68e0bab076568da7c6
zigext <zigext@gmail.com>
2017-06-29 11:24:47 +0700

    test + use jest --coverage

--- server/node/__test__/summary.test.js
+++ server/node/__test__/summary.test.js
@@ -1,1 +1,26 @@
 const summary = require('../summary')
+
+
+describe('calculateTotalScore', () => {
+    it('calculate total score of all questions', () => {
+        expect(summary.calculateTotalScore(10)).toBe(150)
+    })
+}) 
+
+describe('calculateScoreForThatQuestion', () => {
+    test('calculate score for that question => Correct , duration < 1min, point 10', () => {
+        expect(summary.calculateScoreForThatQuestion(10, true, 4806)).toBe(15)
+    })
+
+    test('calculate score for that question => Correct , duration > 1min, point 10', () => {
+        expect(summary.calculateScoreForThatQuestion(10, true, 70000)).toBe(13)
+    })
+
+    test('calculate score for that question => Wrong , duration < 1min, point 10', () => {
+        expect(summary.calculateScoreForThatQuestion(10, false, 4806)).toBe(0)
+    })
+
+    test('calculate score for that question => Wrong , duration > 1min, point 10', () => {
+        expect(summary.calculateScoreForThatQuestion(10, false, 70000)).toBe(0)
+    })
+})
