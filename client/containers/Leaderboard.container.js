5062b581f76c5f85170b438ef1ba6137a4deacdb
mtreerungroj <m.treerungroj@outlook.com>
2017-07-03 13:38:14 +0700

    Watch developer data at Leaderboard container

--- client/containers/Leaderboard.container.js
+++ client/containers/Leaderboard.container.js
@@ -1,5 +1,12 @@
 
 import Leaderboard from '../components/Leaderboard'
 import withFiredux from '../libs/withFiredux'
+import * as Actions from '../actions'
+import store from '../store'
+import firedux from '../store/firedux'
+
+firedux.watch('Developer').then(() => {
+  store.dispatch(Actions.getDeveloper())
+})
 
 export default withFiredux(Leaderboard)
