be9aec9f536ccb2c3af2d295d065f4526f0825e7
mtreerungroj <m.treerungroj@outlook.com>
2017-07-04 17:34:58 +0700

    Test leaderboard component

--- client/components/__test__/Leaderboard.spec.js
+++ client/components/__test__/Leaderboard.spec.js
@@ -2,36 +2,26 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import { Leaderboard, LeaderboardItem } from '../Leaderboard'
 
+const props = {
+  developers: [{
+    profile: {
+      first_name: 'Rungsikorn'
+    },
+    maxSummary: {
+      grade: 'B',
+      score: 75
+    }
+  }]
+}
+
 describe('Leaderboard component test', () => {
 
-  it('should render leaderbaord item message'), () => {
-    const props = {
-      developers: [{
-        profile: {
-          first_name: 'Rungsikorn'
-        },
-        maxSummary: {
-          grade: 'B',
-          score: 75
-        }
-      }],
-    }
+  it('should render leaderbaord item message', () => {
     const Wrapper = shallow(<LeaderboardItem developer={props.developers[0]} key={0} idx={0} />)
-    expect(Wrapper.contains('1. Rungsikorn B ( score: 75 )')).toEqual(true)
-  }
+    expect(Wrapper.contains(<h2>1. Rungsikorn B ( score: 75 )</h2>)).toEqual(true)
+  })
 
   it('should render leaderboard component', () => {
-    const props = {
-      developers: [{
-        profile: {
-          first_name: 'Rungsikorn'
-        },
-        maxSummary: {
-          grade: 'B',
-          score: 75
-        }
-      }],
-    }
     const Wrapper = shallow(<Leaderboard {...props} />)
     expect(Wrapper.contains(<h2>Leaderboard</h2>)).toEqual(true)
     expect(Wrapper.contains(<LeaderboardItem developer={props.developers[0]} key={0} idx={0} />)).toEqual(true)
