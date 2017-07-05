8a6ef8b2b260b3650f1d5cfdb2ea139d61b030dd
zigext <zigext@gmail.com>
2017-07-05 12:36:26 +0700

    test

---
+++ server/node/__test__/firebase.test.js
@@ -0,0 +1,21 @@
+const firebase = require('../firebase')
+
+// describe('connect to firebase', () => {
+//     test('connect to firebase', () => {
+       
+//     })
+// })
+
+describe('number of questions', () => {
+    test('get number of questions', () => {
+       expect.assertions(1)
+       return expect(firebase.getNumberOfQuestions()).resolves.toBeDefined()
+    })
+})
+
+describe('questions from firebase', () => {
+    test('get all questions keys from firebase', () => {
+       expect.assertions(1)
+       return expect(firebase.getNumberOfQuestions()).resolves.toBeDefined()
+    })
+})
