d15c62aaf37252ba370f3d2a127e624b19e94525
mtreerungroj <m.treerungroj@outlook.com>
2017-07-05 17:34:23 +0700

    Code Refactoring

--- client/components/protected/QuizItem.js
+++ client/components/protected/QuizItem.js
@@ -2,40 +2,42 @@ import React, { Component, PropTypes } from 'react'
 import classnames from 'classnames'
 import QuizInput from './QuizInput'
 
+export const Element = ({ quest, deleteQuiz, editQuiz }) => {
+  function handleSave(id, quiz, isChoice) {
+    editQuiz(`${id}`, quiz, isChoice)
+  }
+
+  return (
+    <div>
+      <QuizInput
+        quest={quest}
+        onSave={(quiz, isChoice) => handleSave(quest.id, quiz, isChoice)} />
+      <button
+        className="destroy"
+        onClick={() => deleteQuiz(quest.id)} />
+    </div>
+  )
+}
+
 class QuizItem extends Component {
   constructor(props, context) {
     super(props, context)
   }
 
-  handleSave(id, quiz, isChoice) {
-    this.props.editQuiz(`${id}`, quiz, isChoice)
-  }
-
   render() {
-    const { quest, deleteQuiz } = this.props
-    let element = (
-      <div>
-        <QuizInput
-          quest={quest}
-          onSave={(quiz, isChoice) => this.handleSave(quest.id, quiz, isChoice)} />
-        <button
-          className="destroy"
-          onClick={() => deleteQuiz(quest.id)} />
-      </div>
-    )
-
+    const { quest, deleteQuiz, editQuiz } = this.props
     return (
       <li className={classnames({ completed: quest.completed })}>
-        {element}
+        <Element quest={quest} deleteQuiz={deleteQuiz} editQuiz={editQuiz} />
       </li>
     )
   }
 }
 
 QuizItem.propTypes = {
-  quest: PropTypes.object.isRequired,
-  editQuiz: PropTypes.func.isRequired,
-  deleteQuiz: PropTypes.func.isRequired,
+  quest: PropTypes.object,
+  editQuiz: PropTypes.func,
+  deleteQuiz: PropTypes.func,
 }
 
 export default QuizItem
