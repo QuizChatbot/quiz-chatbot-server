2af1aa1c05fd6893a07eb05b3b5f90bd9ada470a
zigext <zigext@gmail.com>
2017-07-06 18:22:45 +0700

    test

--- server/node/messenger.js
+++ server/node/messenger.js
@@ -1,4 +1,38 @@
 const request = require('request')
+const config = require('config')
+
+let APP_SECRET, VALIDATION_TOKEN, PAGE_ACCESS_TOKEN, SERVER_URL
+
+try {
+  // App Secret can be retrieved from the App Dashboard
+  APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
+    process.env.MESSENGER_APP_SECRET :
+    config.get('appSecret')
+
+  // Arbitrary value used to validate a webhook
+  VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+    (process.env.MESSENGER_VALIDATION_TOKEN) :
+    config.get('validationToken')
+
+  // Generate a page access token for your page from the App Dashboard
+  PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+    (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+    config.get('pageAccessToken')
+
+  // URL where the app is running (include protocol). Used to point to scripts and 
+  // assets located at this address. 
+  SERVER_URL = (process.env.SERVER_URL) ?
+    (process.env.SERVER_URL) :
+    config.get('serverURL')
+
+  if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
+    console.error("Missing config values")
+    process.exit(1)
+  }
+
+} catch (error) {
+  console.warn(error)
+}
 
 function sendTextMessage(recipientId, messageText) {
   let messageData = {
