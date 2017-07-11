bcf12864c2f9836d1f8a2c2fb8d2a05d31428a1b
zigext <zigext@gmail.com>
2017-07-11 17:26:42 +0700

    test

--- server/node/app.js
+++ server/node/app.js
@@ -29,9 +29,6 @@ let APP_SECRET, VALIDATION_TOKEN, PAGE_ACCESS_TOKEN, SERVER_URL
 
 const app = async () => {
 
-  let t = await firebase.getAllAnswersFromQuestion('y')
-  console.log("____t= ", t)
-
   // config.serverURL = tunnelConfig.serverURL
   // console.log("config ", config, tunnelConfig)
 
