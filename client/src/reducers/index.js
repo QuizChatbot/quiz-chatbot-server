a4acfab67f0c415e07fa78d2a86052afaa8b6f5f
mtreerungroj <m.treerungroj@outlook.com>
2017-07-13 11:27:43 +0700

    Create categoryData reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux'
 import firedux from '../store/firedux'
 import developerData from './developerData'
 import questData from './questData'
+import categoryData from './categoryData'
 import app from './app'
 
 const rootReducer = combineReducers({
   firedux: firedux.reducer(),
   developerData,
   questData,
+  categoryData,
   app
 })
 
