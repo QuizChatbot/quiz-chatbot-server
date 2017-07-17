959b6f436aacd396f1742c2cdb15e9f42cc4d28c
zigext <zigext@gmail.com>
2017-07-17 11:21:35 +0700

    test ua

--- server/node/app.js
+++ server/node/app.js
@@ -19,8 +19,7 @@ const
   config = require('./config'),
   emitter = require('./emitter'),
   analytics = require('./analytics'),
-  ua = require('universal-analytics'),
-  uga = require('universal-ga')
+  ua = require('universal-analytics')
 
 
 let APP_SECRET, VALIDATION_TOKEN, PAGE_ACCESS_TOKEN, SERVER_URL, UNIVERSAL_ANALYTICS
