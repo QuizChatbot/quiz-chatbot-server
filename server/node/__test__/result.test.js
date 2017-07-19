ba9211f096267e2d879932ff91996ec1ae4a2b71
zigext <zigext@gmail.com>
2017-07-19 14:15:07 +0700

    fix test

--- server/node/__test__/result.test.js
+++ server/node/__test__/result.test.js
@@ -1,6 +1,6 @@
 const result = require('../result')
 
-describe.skip('prepare result to save to firebase', () => {
+describe('prepare result to save to firebase', () => {
     test('prepare result', () => {
 
         expect.assertions(1)
