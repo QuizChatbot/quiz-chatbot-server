60f11522c22f0a50bb1614a9279ebccab7f5943c
zigext <zigext@gmail.com>
2017-07-19 14:22:30 +0700

    fix test

--- server/node/__test__/result.test.js
+++ server/node/__test__/result.test.js
@@ -11,14 +11,14 @@ describe('prepare result to save to firebase', () => {
             point: 10 
         }
 
-        let prepareResult = result.prepareResultForFirebase(payload, ['a1', 'a2', 'a3'], 2,
-            true, '2017-07-04T18:08:49+07:00', 1499167085389, 15, '00001', '12facts')
+        let prepareResult = result.prepareResultForFirebase(payload, 5, true, , '2017-07-04T18:08:49+07:00',
+        ['a1', 'a2', 'a3'], 1499167085389, 15, '00001', '12facts')
 
         let expected = [{
             answer: 'a1',
             point: 10,
             question: 'question',
-            round: 2,
+            round: 5,
             score: 15,
             result: true,
             startedAt: '2017-07-04T18:08:49+07:00',
